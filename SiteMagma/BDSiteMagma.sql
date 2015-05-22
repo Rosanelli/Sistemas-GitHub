3307e0c16bf2e4ff991104088aee8edddea8f095
Rosanelli <alan_rosaneli@hotmail.com>
2015-05-22 11:03:00 -0300

    update

--- SiteMagma/BDSiteMagma.sql
+++ SiteMagma/BDSiteMagma.sql
@@ -7,3 +7,25 @@ create table NOTICIA(
  Constraint PK_idnoticia primary key(id),
  Constraint UK_titulonoticia unique(titulo)
 )
+
+-------------------------------------------------
+tabelas atualizadas
+
+create table produto (  
+ id int identity,
+ imagem varchar(300),
+ imagemZoom varchar(300),
+ descricao varchar(120),
+ aplicacao varchar(500),
+ idgrupo int,
+ Constraint PK_idproduto primary key(id),
+ Constraint FK_idproduto foreign key(idgrupo) references grupo_produto(id)
+)
+
+create table grupo_produto(
+  id int identity,
+  descricao varchar(50),
+  Constraint PK_idgrupo_produto primary key(id)   
+)
+
+select * from produto
